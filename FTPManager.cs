df8482b2ed7a9d184de60d83e41c0e9ccc6b7b94
wash_rotom <wash_rotom@naver.com>
2023-02-22 11:33:17 +0900

    FRIST EDIT

---
+++ FTPManager.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+using System.Linq;
+using System.Net;
+using System.Threading.Tasks;
+
+namespace FTP
+{
+    class FTPManager
+    {
+        public delegate void ExceptionEventHander(string LocationD, Exception ex);
+        public event ExceptionEventHander ExceptionEvent;
+
+        public Exception Exception = null;
+
+        public bool IsConnected { get; set; }
+
+        private string ipAddr = string.Empty;
+        private string port = string.Empty;
+        private string userld = string.Empty;
+
+        public FTPManager()
+        {
+
+        }
+
+
+    }
+}
