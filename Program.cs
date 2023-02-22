4694aafcd306204798e62c54321292437476a749
wash_rotom <wash_rotom@naver.com>
2023-02-22 14:04:37 +0900

    THREE EDIT

--- Program.cs
+++ Program.cs
@@ -17,7 +17,32 @@ namespace FTP
             Application.SetHighDpiMode(HighDpiMode.SystemAware);
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
+
+            MainFTP();
+
             Application.Run(new Form1());
+        }
+
+        static void MainFTP(string addr, string user, string pwd, string port)
+        {
+            /*string addr = string.Empty;
+            string user = string.Empty;
+            string pwd = string.Empty;
+            string port = string.Empty;*/
+
+            FTPManager manager = new FTPManager();
+
+            bool result = manager.ConnectToServer(addr, port, user, pwd);
+
+            if(result == true)
+            {
+                MessageBox.Show("FTP 立加己傍");
+            }
+            else
+            {
+                MessageBox.Show("FTP 立加角菩");
+            }
+
         }
     }
 }
